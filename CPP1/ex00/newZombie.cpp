339fe29cb83006ea0bdf7a239fb1f3c79b1b67ac
PilSenior <clear@TEKDEMIR.localdomain>
2026-01-24 16:14:43 +0300

    allow

--- CPP1/ex00/newZombie.cpp
+++
@@ -1,7 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie(const std::string& name) {
-    // Heap'te yer ayırır ve pointer döndürür.
-    // Bu zombiyi çağıran kişi (main), onu manuel olarak silmek zorundadır.
-    return (new Zombie(name));
-}
